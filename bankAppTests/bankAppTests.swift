2994d0f50ba3720d89bbda8e1abf513224262b19
gilles willemot <gilles.willemot@gmail.com>
2019-06-17 15:25:45 +0200

    add bankApp-ios to github

---
+++ bankAppTests/bankAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  bankAppTests.swift
+//  bankAppTests
+//
+//  Created by gilles willemot on 09/05/2019.
+//  Copyright © 2019 gilles willemot. All rights reserved.
+//
+
+import XCTest
+@testable import bankApp
+
+class bankAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
