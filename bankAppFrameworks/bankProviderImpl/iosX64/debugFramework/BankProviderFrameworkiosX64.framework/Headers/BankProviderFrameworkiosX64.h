69662da4e048f9aff93830a61a821333601c4fc5
user156869 <user156869@92-63-141-229.rdns.melbourne.co.uk>
2019-08-06 12:49:32 +0100

    core and bankProvider frameworks added

---
+++ bankAppFrameworks/bankProviderImpl/iosX64/debugFramework/BankProviderFrameworkiosX64.framework/Headers/BankProviderFrameworkiosX64.h
@@ -0,0 +1,177 @@
+#import <Foundation/NSArray.h>
+#import <Foundation/NSDictionary.h>
+#import <Foundation/NSError.h>
+#import <Foundation/NSObject.h>
+#import <Foundation/NSSet.h>
+#import <Foundation/NSString.h>
+#import <Foundation/NSValue.h>
+
+@class BPFXBank;
+
+@protocol BPFXIBankProvider;
+
+NS_ASSUME_NONNULL_BEGIN
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-warning-option"
+#pragma clang diagnostic ignored "-Wnullability"
+#pragma clang diagnostic ignored "-Wnullability-completeness"
+#pragma clang diagnostic ignored "-Wswift-name-attribute"
+
+@interface KotlinBase : NSObject
+- (instancetype)init __attribute__((unavailable));
++ (instancetype)new __attribute__((unavailable));
++ (void)initialize __attribute__((objc_requires_super));
+@end;
+
+@interface KotlinBase (KotlinBaseCopying) <NSCopying>
+@end;
+
+__attribute__((objc_runtime_name("KotlinMutableSet")))
+__attribute__((swift_name("KotlinMutableSet")))
+@interface BPFXMutableSet<ObjectType> : NSMutableSet<ObjectType>
+@end;
+
+__attribute__((objc_runtime_name("KotlinMutableDictionary")))
+__attribute__((swift_name("KotlinMutableDictionary")))
+@interface BPFXMutableDictionary<KeyType, ObjectType> : NSMutableDictionary<KeyType, ObjectType>
+@end;
+
+@interface NSError (NSErrorKotlinException)
+@property (readonly) id _Nullable kotlinException;
+@end;
+
+__attribute__((objc_runtime_name("KotlinNumber")))
+__attribute__((swift_name("KotlinNumber")))
+@interface BPFXNumber : NSNumber
+- (instancetype)initWithChar:(char)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedChar:(unsigned char)value __attribute__((unavailable));
+- (instancetype)initWithShort:(short)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedShort:(unsigned short)value __attribute__((unavailable));
+- (instancetype)initWithInt:(int)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedInt:(unsigned int)value __attribute__((unavailable));
+- (instancetype)initWithLong:(long)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedLong:(unsigned long)value __attribute__((unavailable));
+- (instancetype)initWithLongLong:(long long)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedLongLong:(unsigned long long)value __attribute__((unavailable));
+- (instancetype)initWithFloat:(float)value __attribute__((unavailable));
+- (instancetype)initWithDouble:(double)value __attribute__((unavailable));
+- (instancetype)initWithBool:(BOOL)value __attribute__((unavailable));
+- (instancetype)initWithInteger:(NSInteger)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedInteger:(NSUInteger)value __attribute__((unavailable));
++ (instancetype)numberWithChar:(char)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedChar:(unsigned char)value __attribute__((unavailable));
++ (instancetype)numberWithShort:(short)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedShort:(unsigned short)value __attribute__((unavailable));
++ (instancetype)numberWithInt:(int)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedInt:(unsigned int)value __attribute__((unavailable));
++ (instancetype)numberWithLong:(long)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedLong:(unsigned long)value __attribute__((unavailable));
++ (instancetype)numberWithLongLong:(long long)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedLongLong:(unsigned long long)value __attribute__((unavailable));
++ (instancetype)numberWithFloat:(float)value __attribute__((unavailable));
++ (instancetype)numberWithDouble:(double)value __attribute__((unavailable));
++ (instancetype)numberWithBool:(BOOL)value __attribute__((unavailable));
++ (instancetype)numberWithInteger:(NSInteger)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedInteger:(NSUInteger)value __attribute__((unavailable));
+@end;
+
+__attribute__((objc_runtime_name("KotlinByte")))
+__attribute__((swift_name("KotlinByte")))
+@interface BPFXByte : BPFXNumber
+- (instancetype)initWithChar:(char)value;
++ (instancetype)numberWithChar:(char)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinUByte")))
+__attribute__((swift_name("KotlinUByte")))
+@interface BPFXUByte : BPFXNumber
+- (instancetype)initWithUnsignedChar:(unsigned char)value;
++ (instancetype)numberWithUnsignedChar:(unsigned char)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinShort")))
+__attribute__((swift_name("KotlinShort")))
+@interface BPFXShort : BPFXNumber
+- (instancetype)initWithShort:(short)value;
++ (instancetype)numberWithShort:(short)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinUShort")))
+__attribute__((swift_name("KotlinUShort")))
+@interface BPFXUShort : BPFXNumber
+- (instancetype)initWithUnsignedShort:(unsigned short)value;
++ (instancetype)numberWithUnsignedShort:(unsigned short)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinInt")))
+__attribute__((swift_name("KotlinInt")))
+@interface BPFXInt : BPFXNumber
+- (instancetype)initWithInt:(int)value;
++ (instancetype)numberWithInt:(int)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinUInt")))
+__attribute__((swift_name("KotlinUInt")))
+@interface BPFXUInt : BPFXNumber
+- (instancetype)initWithUnsignedInt:(unsigned int)value;
++ (instancetype)numberWithUnsignedInt:(unsigned int)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinLong")))
+__attribute__((swift_name("KotlinLong")))
+@interface BPFXLong : BPFXNumber
+- (instancetype)initWithLongLong:(long long)value;
++ (instancetype)numberWithLongLong:(long long)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinULong")))
+__attribute__((swift_name("KotlinULong")))
+@interface BPFXULong : BPFXNumber
+- (instancetype)initWithUnsignedLongLong:(unsigned long long)value;
++ (instancetype)numberWithUnsignedLongLong:(unsigned long long)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinFloat")))
+__attribute__((swift_name("KotlinFloat")))
+@interface BPFXFloat : BPFXNumber
+- (instancetype)initWithFloat:(float)value;
++ (instancetype)numberWithFloat:(float)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinDouble")))
+__attribute__((swift_name("KotlinDouble")))
+@interface BPFXDouble : BPFXNumber
+- (instancetype)initWithDouble:(double)value;
++ (instancetype)numberWithDouble:(double)value;
+@end;
+
+__attribute__((objc_runtime_name("KotlinBoolean")))
+__attribute__((swift_name("KotlinBoolean")))
+@interface BPFXBoolean : BPFXNumber
+- (instancetype)initWithBool:(BOOL)value;
++ (instancetype)numberWithBool:(BOOL)value;
+@end;
+
+__attribute__((swift_name("IBankProvider")))
+@protocol BPFXIBankProvider
+@required
+- (NSArray<BPFXBank *> *)getBanks __attribute__((swift_name("getBanks()")));
+@end;
+
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("BankProvider")))
+@interface BPFXBankProvider : KotlinBase <BPFXIBankProvider>
+- (instancetype)init __attribute__((swift_name("init()"))) __attribute__((objc_designated_initializer));
++ (instancetype)new __attribute__((availability(swift, unavailable, message="use object initializers instead")));
+- (NSArray<BPFXBank *> *)getBanks __attribute__((swift_name("getBanks()")));
+@end;
+
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("Bank")))
+@interface BPFXBank : KotlinBase
+- (instancetype)initWithName:(NSString *)name __attribute__((swift_name("init(name:)"))) __attribute__((objc_designated_initializer));
+@property NSString *name __attribute__((swift_name("name")));
+@end;
+
+#pragma clang diagnostic pop
+NS_ASSUME_NONNULL_END
